c0277985097ee2288f1e62fbf4ee474b85d04374
Mohit <73020048+MOHIT-4o4@users.noreply.github.com>
2022-06-11 23:14:48 +0530

    leetcode

--- remove_duplicate.cpp
+++ remove_duplicate.cpp
@@ -5,7 +5,6 @@ Return k after placing the final result in the first k slots of nums
 
 
 Example 1:
-
 Input: nums = [3,2,2,3], val = 3
 Output: 2, nums = [2,2,_,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 2.
